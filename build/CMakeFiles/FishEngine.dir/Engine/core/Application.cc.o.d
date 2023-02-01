44dc3090a638a5cda85041cd240d59e3419b102d
Hilal-Almoqbali <hilalalmoqbali2060@gmail.com>
2023-02-01 22:43:30 +0400

    I found better way so I start agine

--- build/CMakeFiles/FishEngine.dir/Engine/core/Application.cc.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/FishEngine.dir/Engine/core/Application.cc.o: \
- /home/hilal/GitHub\ Projects/Fish-Engine/Engine/core/Application.cc \
- /usr/include/stdc-predef.h \
- /home/hilal/GitHub\ Projects/Fish-Engine/Engine/core/Application.h
