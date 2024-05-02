d8ff9a3b24b6da490bfb65f38b6daa77351f0b12
Hilal-Almoqbali <72453298+Hilal-Almoqbali@users.noreply.github.com>
2024-05-02 21:49:21 +0400

    api update

--- src/Engine/renderer/opengl-pipeline/Camera.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/vec2.hpp>
-
-class 
--- src/Engine/renderer/renderer.cpp
+++ src/Engine/renderer/renderer.cpp
@@ -2,5 +2,28 @@
 
 namespce rose
 {
+    Renderer::Renderer()
+    {
 
+    }
+    void Renderer::add_seen(int ID,std::string name, Model model)
+    {
+
+    }
+    void Renderer::draw()
+    {
+
+    }
+    void Renderer::rm_opject_by_id(int ID)
+    {
+
+    }
+    void Renderer::rm_opject_by_name(std::string name)
+    {
+
+    }
+    void Renderer::rm_seen()
+    {
+
+    }
 }
